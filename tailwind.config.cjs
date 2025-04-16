f1d35f3c7f5aadde80dbbe7d2313cdabd8865b78
kegcheck <65734212+kegcheck@users.noreply.github.com>
2025-04-15 21:33:59 -0500

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#3B82F6',
+          secondary: '#EC4899',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
